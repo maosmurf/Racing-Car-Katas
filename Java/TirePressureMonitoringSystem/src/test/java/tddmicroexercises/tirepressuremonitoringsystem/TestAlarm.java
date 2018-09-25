c44a9a8be097d1ca8801ff037f02724ca52dcb1e
Adam Zielinski <adam.zielinski@kurier.at>
2018-09-25 18:20:52 +0200

    uncheckedAlarmIsOff

--- Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.java
+++ Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 public class TestAlarm {
 
     @Test
-    public void foo() {
+    public void uncheckedAlarmIsOff() {
         Alarm alarm = new Alarm();
         assertEquals(false, alarm.isAlarmOn());
     }
